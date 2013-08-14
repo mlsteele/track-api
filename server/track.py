67a31aabcdcebdbac0c53a989362a2b43e27ce0f
Miles Steele <msteele@edx.org>
2013-08-14 11:40:30 -0400

    add server comments

--- server/track.py
+++ server/track.py
@@ -3,6 +3,12 @@ from pprint import pprint
 
 
 class Event(object):
+    """
+    Tracking event.
+
+    Can be converted to json for storage with `.to_json`.
+    Includes a boolean `validated` which defaults to False.
+    """
     def __init__(self, name, data, context):
         self.name = name
         self.data = data
@@ -12,6 +18,7 @@ class Event(object):
         self.context = self.context
 
     def to_json(self):
+        """ Convert to json. """
         return {
             'name': self.name,
             'timestamp': self.timestamp,
@@ -22,6 +29,16 @@ class Event(object):
 
 
 class EventSchema(object):
+    """
+    Schema for an event.
+
+    Contains validators for fields in the event.
+    EventSchema is meant to be used by a `with` block like this:
+        with track.register("Viewed sequential") as evt:
+            evt.require_data('seq_name', kind=str)
+    where track.register returns an `EventSchema`.
+    """
+
     def __init__(self, event_name):
         self.event_name = event_name
         self.data_validators = {}
@@ -44,6 +61,13 @@ class EventSchema(object):
         return validators
 
     def require_data(self, field_name, kind=None, choices=None, validator=None):
+        """
+        Require `field_name` to be present in the data.
+        Further validation keywords are optional.
+        `kind` is a type of which the value must be `isinstance`.
+        `choices` is a collection which the field value must be in.
+        `validator` is a function which is passed the field value and must return a boolean.
+        """
         self.data_validators[field_name] = self._create_validators(
             self.data_validators.get(field_name, []),
             kind=kind,
@@ -52,6 +76,13 @@ class EventSchema(object):
         )
 
     def require_context(self, field_name, kind=None, choices=None, validator=None):
+        """
+        Require `field_name` to be present in the context.
+        Further validation keywords are optional.
+        `kind` is a type of which the value must be `isinstance`.
+        `choices` is a collection which the field value must be in.
+        `validator` is a function which is passed the field value and must return a boolean.
+        """
         self.context_validators[field_name] = self._create_validators(
             self.context_validators.get(field_name, []),
             kind=kind,
@@ -60,6 +91,12 @@ class EventSchema(object):
         )
 
     def validate(self, evt):
+        """
+        Check whether an event passes validation according to this schema.
+
+        Returns a boolean.
+        """
+        # validate data
         for field_name in self.data_validators:
             for validator in self.data_validators[field_name]:
                 if not field_name in evt.data:
@@ -67,6 +104,7 @@ class EventSchema(object):
                 if not validator(evt.data[field_name]):
                     return False
 
+        # validate context
         for field_name in self.context_validators:
             if not field_name in evt.context:
                 return False
@@ -74,10 +112,13 @@ class EventSchema(object):
                 if not validator(evt.context[field_name]):
                     return False
 
+        # TODO detect extra fields
+
         return True
 
 
 class Track(object):
+    """ Event tracking host. """
     def __init__(self):
         self.context = {}
         self.registered_events = {}
@@ -89,9 +130,17 @@ class Track(object):
         self.context = {}
 
     def update_context(self, more_context):
+        """ Add additional context or update existing context fields. """
         self.context.update(more_context)
 
     def event(self, event_name, event_data):
+        """
+        Log an event.
+
+        `event_name` is the type of the event as a string.
+        `event_data` is the event payload.
+        context is from this `Track` is added to the event.
+        """
         evt = Event(event_name, event_data, self.context)
         if evt.name in self.registered_events:
             schema = self.registered_events[evt.name]
@@ -104,6 +153,13 @@ class Track(object):
         pprint(evt.to_json())
 
     def register(self, event_name):
+        """
+        Register an event type.
+
+        Used by a with block like this:
+            with track.register("Viewed sequential") as evt:
+                evt.require_data('seq_name', kind=str)
+        """
         schema = EventSchema(event_name)
         self.registered_events[event_name] = schema
         return schema
