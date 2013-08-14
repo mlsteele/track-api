842d6af5f7bc23b2a91067577e47db456bcfdea5
Miles Steele <msteele@edx.org>
2013-08-14 10:41:58 -0400

    change example

--- usage.py
+++ usage.py
@@ -2,10 +2,10 @@ from track import default as track
 
 
 # -- event registration --
-with track.register("Lost socks") as evt:
-    evt.require_data('color', kind=str)
-    evt.require_data('smelly', choices=['nope', 'somewhat', 'very'])
-    evt.require_data('days_old', validator=lambda x: x > 1)
+with track.register("Viewed sequential") as evt:
+    evt.require_data('seq_name', kind=str)
+    evt.require_data('referrer', choices=['prev', 'next', 'other'])
+    evt.require_data('seq_index', validator=lambda x: x > 1)
     evt.require_context('username')
 
 
@@ -14,8 +14,8 @@ track.set_context({
     'username': 'Miles'
 })
 
-track.event("Lost socks", {
-    'color': 'blue',
-    'smelly': 'somewhat',
-    'days_old': 4,
+track.event("Viewed sequential", {
+    'seq_name': 'electrons',
+    'referrer': 'prev',
+    'seq_index': 4,
 })
